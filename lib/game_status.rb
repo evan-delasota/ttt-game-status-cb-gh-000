6712545c88c4d02404ff7c39b49a7c9ed760d6e7
Evan Delasota <evan.delasota@gmail.com>
2020-03-01 21:01:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,7 +10,7 @@ WIN_COMBINATIONS = [
   ]
 
 def won?(board)
-  board.each do |win_combination|
+  for each win_combination in WIN_COMBINATIONS
       win_index_1 = win_combination[0]
       win_index_2 = win_combination[1]
       win_index_3 = win_combination[2]
